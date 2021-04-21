838976832f36a37df4c721206e32f1cd15dd043d
Juniorr452 <juniorr452@gmail.com>
2021-04-21 14:22:51 -0300

    finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+
+import { Button } from '../components/Button';
+
+import '../styles/sidebar.scss';
+
+export interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  onButtonClick: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, onButtonClick}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onButtonClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
